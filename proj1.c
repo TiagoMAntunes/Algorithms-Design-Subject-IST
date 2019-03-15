6efccec4c003937600bb92e8aa8369dd3d2cc885
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-03-15 15:07:44 +0000

    Changed DFS to use flag

--- proj1.c
+++ proj1.c
@@ -10,6 +10,7 @@
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 #define JUSTDOIT 1
 #define DONTDOIT 0
+#define YELLOW 	3
 int time;
 
 Net read_input() {
@@ -49,6 +50,7 @@ int DFS_visit(Net net, Item u, int low[], int do_it) {
 	int val = u->_id;
 	for (i = 0; i < vector_size(adjs_vector); i++) {		
 		Item v = vector_at(adjs_vector,i);
+
 		if (v->_color == WHITE) {
 			children++;
 			v->_pi = u->_id;
@@ -59,19 +61,24 @@ int DFS_visit(Net net, Item u, int low[], int do_it) {
 
 	        /*  u is not root and low of one of its children is more than u's */
 	        if (do_it && u->_pi != NIL && low[v->_id -1] >= u->_d) {
+	        	u->_color = YELLOW;
 	           	net_add_art_point(net, u);
 	        }
         
             /*  u is root and has 2 or more children */
 	        if (do_it && u->_pi == NIL && children > 1) {
+	        	u->_color = YELLOW;
 	          	net_add_art_point(net, u);
 	         }
 
 		} else if (v->_id != u->_pi) { /* if the vertex isn't where it came from */
             low[u->_id -1] = MIN(low[u->_id - 1], v->_d);
         }
+
 	}
-	u->_color = BLACK;
+	if (do_it && u->_color != YELLOW) {
+		u->_color = BLACK;
+	}
 	time++;
 	u->_f = time;
 
@@ -86,16 +93,20 @@ Vector DFS(Net net, int do_it) {
     int * low = calloc(sizeof(int),N);
 	Vector subnets_maxs = create_vector(1);
  
-	for (i = 0; i < N; i++) {		
-		items[i]->_color = WHITE;
+	for (i = 0; i < N; i++) {	
+		if (do_it || (!do_it && items[i]->_color != YELLOW)) {	
+			items[i]->_color = WHITE;
+		}
+
 		items[i]->_pi = NIL;
 		low[i] = NIL;
 	}
 
 	time = 0;
 	for (i = 0; i < N; i++) {		
-		if (items[i]->_color == WHITE)
+		if (items[i]->_color == WHITE) {
 			vector_push_back(subnets_maxs, create_item(DFS_visit(net, items[i], low, do_it)));
+		}
 	}
 	free(low);
 	return subnets_maxs;
@@ -125,19 +136,20 @@ int main() {
 	int i, biggest_size = 0;
 	Net net = read_input();   
 	Vector subnets_net = DFS(net,JUSTDOIT);
-	printf("First DFS done\n");
+//	printf("First DFS done\n");
 	/*  build new net without the ap's and do the DFS */
-	Net new_net = net_create_remove_articulations(net);
-	printf("Updated graph\n");
-	delete_vector(DFS(new_net,DONTDOIT), delete_item);
-	printf("Second DFS done\n");
-	for (i = 0; i < new_net->_n_routers; i++) {		
-		Item item = vector_at(new_net->_routers_vec,i);
-		if (item->_pi == NIL)
+	//Net new_net = net_create_remove_articulations(net);
+	//printf("Updated graph\n");
+
+	delete_vector(DFS(net,DONTDOIT), delete_item);
+	//printf("Second DFS done\n");
+	for (i = 0; i < net->_n_routers; i++) {		
+		Item item = vector_at(net->_routers_vec,i);
+		if (item->_pi == NIL && item->_color != YELLOW)
 			if ((item->_f / 2) - item->_d + 1 > biggest_size)
 				biggest_size = (item->_f / 2) - item->_d + 1;
 	}
-	printf("Found the data needed\n");
+//	printf("Found the data needed\n");
 	print_results(subnets_net, net, biggest_size);
 
 	/* clean up */
