1675c527224f0c941b67888e6ca9a782ec7fc178
Tiago Antunes <tiago.melo.antunes@gmail.com>
2019-04-28 18:06:59 +0100

    Added tests

--- proj2/proj.cpp
+++ proj2/proj.cpp
@@ -8,19 +8,19 @@ int main() {
     while (f-- > 0) {
         int prod;
         std::cin >> prod;
-        std::cout << prod;
+        std::cout << prod << " ";
     }
     std::cout << std::endl;
     while (e-- > 0) {
         int cap;
         std::cin >> cap;
-        std::cout << cap;
+        std::cout << cap << " ";
     }
     std::cout << std::endl;
     while (t-- > 0) {
         int o, d, c;
         std::cin >> o >> d >> c;
-        std::cout << o << d << c;
+        std::cout << o << " " << d << " " << c << " ";
     std::cout << std::endl;
     }
     return 0;
