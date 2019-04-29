9e0936b9acb1b85a067d1d72986c995f9e4e5816
Tiago Antunes <tiago.melo.antunes@gmail.com>
2019-04-29 10:33:36 +0100

    Changed edge. Created basic edges array. TransformID function for getting complementar vertexes

--- proj2/proj.cpp
+++ proj2/proj.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 
+#define SOURCE 1
+
 class Edge{
         int _weight;
         int _target;
     public:
-        Edge(int weight, int target);
         int getWeight() { return _weight; }
         int getTarget() { return _target; }
+        int setWeight(int weight) { _weight = weight; }
+        int setTarget(int target) { _target = target; }
 };
 
-Edge::Edge(int weight, int target) {
-    _weight = weight;
-    _target = target;
+int f, e, t;
+
+int transformID(int id) {
+    if (id >= 2 && id <= f + 1) {
+        //do some logic
+    } else if (id > f + 1) {
+        //do some logic
+    }
 }
 
 int main() {
     //parse input
-    int f, e, t;    
     int prod;
     int cap;
     int o, d, c;
     std::cin >> f >> e >> t;
-    
+
+    Edge * edges = new Edge[2 + f * 2 + e * 2]; //source + target + 2 * suppliers + 2 * stations
+
     while (f-- > 0)
         std::cin >> prod;
         
@@ -31,7 +40,6 @@ int main() {
     while (t-- > 0) 
         std::cin >> o >> d >> c;
 
-
-
+    delete[] edges;
     return 0;
 }
